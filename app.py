c4bea4abad94d24fcff7923b2285630182083d2b
nurfitryah <68369357+nurfitryah@users.noreply.github.com>
2020-08-12 09:49:55 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -109,22 +109,30 @@ def get_data(artistname):
 def documentation():
     return '''
         <h1> Documentation </h1>
-        <p> Repositori ini bertujuan untuk melengkapi Data Analytics Courde, dengan pemilihan case Deploying Application Programming Interface (API).
+        <p> Repositori ini bertujuan untuk melengkapi Data Analytics Course, dengan pemilihan case Deploying Application Programming Interface (API).
         <p> Dalam repositori ini ada dua (2) Static Endpoint dan satu (1) Dynamic Endpoint yang akan dijelaskan di point dibawah. 
         <p> Data source : chinook.db
-        <h2> Static Endpoints 1 (.../chinook) :</h2>
-                <p> Static Endpoints 1 akan menunjukkan kolom ArtistName, Title, Country (customers), GenreName, MediaType, 
+        <h2> Static Endpoints 1 (apiccapstone.herokuapp.com/chinook) :</h2>
+                <p> Static Endpoints 1 bertujuan untuk melihat total penjualan keseluruhan genre dengan artis siapa serta tipe media yg dibeli. 
+                <p> Static Endpoints 1 akan menunjukkan kolom ArtistName, Title, Country (customers), GenreName, MediaType,
                 <p> DatePurchased, Total, dan MonthPurchased. Sebagai contoh digunakan untuk melihat penjualan secara general per bulannya.
-                <p> (contoh insightnya)
-        <li>
-        <h2> Static Endpoints 2 (.../mediatype) :</h2>
+
+        <h2> Static Endpoints 2 (apiccapstone.herokuapp.com/mediatype) :</h2>
                 <p> Static Endpoints 2 dibuat bertujuan untuk melihat total jumlah penjualan musik dengan tipe media per bulannya.
                 <p> Static Endpoints 2 akan menunjukkan kolom MediaType (jenis tipe media yang customer beli) sebagai index dan Total (total pembelian) per bulannya.  
                 <p> Sebagai contoh, Static Endpoint 2 untuk melihat jenis tipe media apa yang paling sering dibeli per bulannya dan perbandingannya.
-        <li>
-        <h2> Dynamic Endpoints (Artist Name) (.../data/get/<artistname>) :</h2>
-                <p> Data dynamic akan menunjukkan data dari nama artis yang dipilih saja. 
-                <p> Misal ingin melihat di Negara apa lagu dari artis U2 lebih sering dibeli atau pada bulan apa lagu dari artis Aerosmith paling banyak dibeli
+
+        <h2> Dynamic Endpoints (Artist Name) (apiccapstone.herokuapp.com/data/get/<artistname>) :</h2>
+                <p> Dynamic Endpoints akan menunjukkan data dari nama artis yang dipilih saja. 
+                <p> Misal ingin melihat di Negara apa lagu dari artis U2 lebih sering dibeli atau pada bulan apa lagu dari artis Aerosmith paling banyak dibeli.
+
+        <p> Untuk mengecek data-data tersebut, anda bisa copy tautan link dibawah ini :
+        <li> Main               : https://apiccapstone.herokuapp.com/
+        <li> Static Endpoints 1 : https://apiccapstone.herokuapp.com/chinook
+        <li> Static Endpoints 2 : https://apiccapstone.herokuapp.com/mediatype
+        <li> Dynamic Endpoint   : https://apiccapstone.herokuapp.com/data/get/<artistname>
+        <p> Contoh khusus untuk Dynamic Endpoint : https://apiccapstone.herokuapp.com/data/get/Queen 
+
     '''
 
 
