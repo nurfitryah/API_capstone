ce12acdeb3cf3144e6cf836c0bbb7112d4f0de32
nurfitryah <68369357+nurfitryah@users.noreply.github.com>
2020-08-18 23:21:37 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -69,6 +69,23 @@ def mediatype():
     sort_values(by='Total', ascending=False).unstack().fillna(0)
     return (data_mediatype.to_json())
 
+#static endpoints 3: (checking artist name)
+@app.route('/artistlist')
+def artistlist():
+    conn = sqlite3.connect('data_input/chinook.db')
+    artist_list = pd.read_sql_query(
+    '''
+    SELECT artists.Name as ArtistName, albums.Title, genres.Name as GenreName 
+    FROM artists
+    LEFT JOIN albums  
+    ON artists.ArtistId = albums.ArtistId
+    LEFT JOIN tracks
+    ON albums.AlbumId = tracks.AlbumId
+    LEFT JOIN genres
+    ON tracks.GenreId = genres.GenreId
+    ''',
+    conn )
+    return (artist_list.to_json())
 
 # dynamic endpoint:
 @app.route('/data/get/<artistname>', methods=['GET'])
@@ -110,7 +127,7 @@ def documentation():
     return '''
         <h1> Documentation </h1>
         <p> Repositori ini bertujuan untuk melengkapi Data Analytics Course, dengan pemilihan case Deploying Application Programming Interface (API).
-        <p> Dalam repositori ini ada dua (2) Static Endpoint dan satu (1) Dynamic Endpoint yang akan dijelaskan di point dibawah. 
+        <p> Dalam repositori ini ada tiga (3) Static Endpoint dan satu (1) Dynamic Endpoint yang akan dijelaskan di point dibawah. 
         <p> Data source : chinook.db
         <h2> Static Endpoints 1 (apiccapstone.herokuapp.com/chinook) :</h2>
                 <p> Static Endpoints 1 bertujuan untuk melihat total penjualan keseluruhan genre dengan artis siapa serta tipe media yg dibeli. 
@@ -122,12 +139,17 @@ def documentation():
                 <p> Static Endpoints 2 akan menunjukkan kolom MediaType (jenis tipe media yang customer beli) sebagai index dan Total (total pembelian) per bulannya.  
                 <p> Sebagai contoh, Static Endpoint 2 untuk melihat jenis tipe media apa yang paling sering dibeli per bulannya dan perbandingannya.
 
+        <h2> Static Endpoints 3 (.../artistlist): </h2>
+                <p> Static Endpoints 3 dibuat bertujuan agar user dapat melihat daftar nama artis yang berada
+                <p> pada data `chinook.db` dan user dapat mengecek apakah data artis pilihannya berada didalam 
+                <p> data `chinook.db`. Static Endpoints 3 akan menunjukkan kolom ArtistName (nama artis yang dipilih),
+                <p> Title (judul album dari masing-masing artis), dan GenreName (genre dari artis tersebut). 
+
         <h2> Dynamic Endpoints (Artist Name) (apiccapstone.herokuapp.com/data/get/<artistname>) :</h2>
                 <p> Dynamic Endpoints akan menunjukkan data dari nama artis yang dipilih saja. 
                 <p> Misal ingin melihat di Negara apa lagu dari artis U2 lebih sering dibeli atau pada bulan apa lagu dari artis Aerosmith paling banyak dibeli.
 
         <p> Untuk mengecek data-data tersebut, anda bisa copy tautan link dibawah ini :
-        <li> Main               : https://apiccapstone.herokuapp.com/
         <li> Static Endpoints 1 : https://apiccapstone.herokuapp.com/chinook
         <li> Static Endpoints 2 : https://apiccapstone.herokuapp.com/mediatype
         <li> Dynamic Endpoint   : https://apiccapstone.herokuapp.com/data/get/<artistname>
